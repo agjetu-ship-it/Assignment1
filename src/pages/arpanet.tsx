69936ab8d44c03b9907e87ef60c70ae0187d64f3
danielzheng9 <95480903+danielzheng9@users.noreply.github.com>
2026-05-04 19:18:45 +1000

    ARPANET image and more info

--- src/pages/arpanet.tsx
+++ src/pages/arpanet.tsx
@@ -2,13 +2,17 @@ import React from "react";
 export function ArpanetPage() {
     return <>
         <main>
-            <img src="https://ethw-images.s3.us-east-va.perf.cloud.ovh.us/ethw/thumb/8/89/ARPANET-Logo.png/300px-ARPANET-Logo.png"/>
+            <img src="https://ethw-images.s3.us-east-va.perf.cloud.ovh.us/ethw/thumb/8/89/ARPANET-Logo.png/300px-ARPANET-Logo.png" alt="A black outline globe icon with the text ARPANET on it."/>
             <h1 className="unique_stripe standard">
-                ARPANET
+                What is ARPANET?
             </h1>
             <p className="ghost">
                 ARPANET also known as Advance Research Project Agency Network is a first wide-area packet-switched network that is distributed control of one of the first computer networks to implement the TCP/IP protocol suite.
             </p>
+            <p className="ghost">
+                ARPANET first began in 1969; established under the Advanced Research Projects Agency, it was the first operational packet-switching network that laid the foundation for the world wide web as we know it today.
+            </p>
+            <img src="https://cdn.hswstatic.com/gif/arpanet-4.jpg" alt="A picture of a guy toggling switches on a board."/>
         </main>
     </>
 }
