1b4a296fe8828caff5a41564a9d54dc80f93c204
danielzheng9 <95480903+danielzheng9@users.noreply.github.com>
2026-05-04 17:12:13 +1000

    homepage

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,16 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 export function Home() {
     return <main>
         <div className="homeStripe">
-            <h1>WELCOME HOME</h1>
+            <h1 className="title">Glossary Solutions and Practical Learning</h1>
+            <NavLink to="/arpanet">Learn about ARPANET</NavLink>
+            <NavLink to="/arpanet">Learn about ARPANET</NavLink>
+            <NavLink to="/arpanet">Learn about ARPANET</NavLink>
+            <NavLink to="/arpanet">Learn about ARPANET</NavLink>
+            <NavLink to="/arpanet">Learn about ARPANET</NavLink>
+            <NavLink to="/arpanet">Learn about ARPANET</NavLink>
+            <NavLink to="/arpanet">Learn about ARPANET</NavLink>
         </div>
     </main>
 }
