a0a6280fc3e5f4dd24031e6a055aa12aa4082cbc
danielzheng9 <95480903+danielzheng9@users.noreply.github.com>
2026-05-07 23:51:51 +1000

    Fixed logo not initialising with correct version

--- src/logo.tsx
+++ src/logo.tsx
@@ -1,15 +1,19 @@
 import { NavLink } from "react-router-dom"
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 export function Logo() {
     const [sourceIcon, setSourceIcon] = useState<string>("https://raw.githubusercontent.com/DOGNET-SUPERB-TECHNOLOGIES/cdn/main/dognetTechnologies.png");
-    useEffect(() => {
-        window.addEventListener('resize', () => {
-            if (window.innerWidth < 600) {
-                setSourceIcon("https://raw.githubusercontent.com/DOGNET-SUPERB-TECHNOLOGIES/cdn/main/favicon.png");
-                return true;
-            }
-            setSourceIcon("https://raw.githubusercontent.com/DOGNET-SUPERB-TECHNOLOGIES/cdn/main/dognetTechnologies.png");
+    const thisSet = useCallback(() => {
+        if (window.innerWidth < 600) {
+            setSourceIcon("https://raw.githubusercontent.com/DOGNET-SUPERB-TECHNOLOGIES/cdn/main/favicon.png");
             return true;
+        }
+        setSourceIcon("https://raw.githubusercontent.com/DOGNET-SUPERB-TECHNOLOGIES/cdn/main/dognetTechnologies.png");
+        return true;
+    }, [])
+    useEffect(() => {
+        thisSet();
+        window.addEventListener('resize', () => {
+            return thisSet();
         });
     }, [])
     return <NavLink to="/" className="heading_logo standard noLink">
