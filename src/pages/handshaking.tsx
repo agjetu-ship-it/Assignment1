9cdfc7b93a45a5a2e8c8b235cc5831c4ddfd84a1
danielzheng9 <95480903+agjetu-ship-it@users.noreply.github.com>
2026-05-12 18:45:05 +1000

    Create handshaking.tsx

---
+++ src/pages/handshaking.tsx
@@ -0,0 +1,12 @@
+export function Handshaking() {
+    return <>
+        <h1>Handshaking</h1>
+        <p>
+            Handshaking is a process where two entities establish a communication link by authenticating the signals from the other. It's a negotiation process where two devices exchange information about each other before the actual process begins.
+        </p>
+        <strong>Three Way Handshake</strong>
+        <p>
+           A three way handshake in Transmission Control Protocol (A Networking Standard) is where the first figure sends an important synchronisation packet to the second figure first, then the second firgure sends a synchronisation packet back ALONG with an acknowledgement packet (a packet telling the other side that the request has been received) (this packet is combined, Synchronisation+Acknowledgement of that packet) back to the first figure. Then, the first figure sends an acknowledgement packet back to the second figure to acknowledge their synchronisation packet.
+        </p>
+    </>
+}
